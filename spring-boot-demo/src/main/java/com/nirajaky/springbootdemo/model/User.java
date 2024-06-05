9b00333624ea545907261b5c25fd7f3432b10da0
Niraj Mehta <niraj86995@gmail.com>
2024-06-05 15:45:51 +0530

    Basic Spring CRUD App

---
+++ spring-boot-demo/src/main/java/com/nirajaky/springbootdemo/model/User.java
@@ -0,0 +1,37 @@
+package com.nirajaky.springbootdemo.model;
+
+public class User {
+    private int ID;
+    private String name;
+    private String address;
+
+    public User(int ID, String name, String address) {
+        this.ID = ID;
+        this.name = name;
+        this.address = address;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void setID(int ID) {
+        this.ID = ID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
